955fab643456c177a4c7fd5127a580ba12114a4c
Tjerk Valentijn <tjerk.valentijn@gmail.com>
2018-11-03 09:30:24 +0100

    progress on filtering

--- src/list/index.js
+++ src/list/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import { push } from "connected-react-router";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import FilterForm from "../filters";
 
-const List = ({ albums, changePage }) => (
-  <div>
+const List = ({ albums, isFiltering, changePage }) => (
+  <article>
+    <div style={isFiltering ? { display: "block" } : { display: "none" }}>
+      filtering ...
+    </div>
     <h1>Albums</h1>
+    <FilterForm />
     <ul>{albums.map(album => <li key={album.id}>{album.name}</li>)}</ul>
-  </div>
+  </article>
 );
 
 const mapStateToProps = state => {
   console.log("MSTP", state);
   return {
-    albums: state.listReducer.albums
+    albums: state.listReducer.albums,
+    isFiltering: state.listReducer.isFiltering
   };
 };
 
